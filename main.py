c7e4c0a3e994bfc34bb3f00fe8a90dfece20e94f
leonardoleandrodev <leonardoleandrodev@gmail.com>
2020-04-23 00:35:29 -0300

    melhorias

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import pygame
 from pygame.locals import *
 import time
 import random
+from playsound import playsound
+
 LARGURA_JANELA = 1024
 ALTURA_JANELA = 768
 FRAMES = 60
@@ -9,10 +11,10 @@ run = True
 fm = 0
 db = ""
 pause = False
-
+timeBall = 0
 lstBola = []
 class Jogador:
-    tamanho = [30,90]
+    tamanho = [30,200]
     posX = 0#posicao largura
     posY = 0#posicao altura
     pontos = 0
@@ -48,10 +50,12 @@ class Bola:
         self.velocidade=2
     def move(self):
 
-        if self.moveUp:
+        if self.moveUp == True:
             self.posY -= self.velocidade
-        else:
+        elif self.moveUp == False:
             self.posY += self.velocidade
+        else:
+            self.posY += 0
         if self.moveLeft:
             self.posX += self.velocidade
         else:
@@ -69,17 +73,25 @@ class Bola:
             self.posInicial()
             self.moveLeft = not self.moveLeft
             jg2.pontos +=1
+           # sound2.play()
         elif (self.posX+self.tamanho[0]) > LARGURA_JANELA:
             self.posInicial()
             self.moveLeft = not self.moveLeft
             jg1.pontos +=1
+            #sound2.play()
         elif self.posY < 0:
             self.moveUp = not self.moveUp
+            #sound1.play()
         elif (self.posY+self.tamanho[1]) > ALTURA_JANELA:
             self.moveUp = not self.moveUp
+            #sound1.play()
         self.move()
             
-
+pygame.mixer.pre_init(44100,-16,1, 1024 * 5)
+pygame.mixer.init()
+pygame.mixer.set_num_channels(10) 
+sound1 = pygame.mixer.Sound('sound1.wav')
+sound2 = pygame.mixer.Sound('sound2.wav')
 
 jg1 = Jogador()
 jg1.cor = (255,0,0)
@@ -91,12 +103,14 @@ jg2 = Jogador()
 jg2.cor = (255,0,255)
 jg2.posX = (LARGURA_JANELA-jg2.tamanho[0]) -10
 jg2.posY = (ALTURA_JANELA/2)-(jg2.tamanho[1]/2)
+for i in range(300) :
+    bola = Bola()
+    bola.cor = (random.randint(1,255),random.randint(1,255),random.randint(1,255))
+    bola.posInicial()
+    lstBola.append(bola)
 
 
-bola = Bola()
-bola.cor = (255,100,0)
-bola.posInicial()
-lstBola.append(bola)
+
 def debug(msg):
     global fontDebug
     text = fontDebug.render(msg, True, [10,10,255])
@@ -119,11 +133,20 @@ def atualizar():
         b.desenhar()
         if b.posY+b.tamanho[1] > jg1.posY and b.posY < jg1.posY+jg1.tamanho[1]:
             if b.posX < jg1.posX+jg1.tamanho[0] and b.posX+b.tamanho[0] > jg1.posX:
-                b.moveLeft = not b.moveLeft
+                b.moveLeft = not b.moveLeft                
                 b.posX+=7
                 b.speedUp()
                 db = clock,b.posX,b.posY,jg1.posY,jg1.posY
                 jg1.pontos-=2
+                if b.posY+b.tamanho[1] < jg1.posY+(jg1.tamanho[1]*0.20):
+                    b.moveUp = True
+                elif b.posY > jg1.posY+(jg1.tamanho[1]*0.80) :
+                    b.moveUp = False
+                else:
+                    b.moveUp = None
+
+                
+                #sound1.play()
         if b.posY+b.tamanho[1] > jg2.posY and b.posY < jg2.posY+jg1.tamanho[1]:
             if b.posX+b.tamanho[0] > jg2.posX and b.posX < jg2.posX+jg2.tamanho[0]:
                 b.moveLeft = not b.moveLeft
@@ -131,6 +154,13 @@ def atualizar():
                 b.speedUp()
                 db = clock,b.posX,b.posY,jg2.posY,jg2.posY
                 jg2.pontos-=2
+                if b.posY+b.tamanho[1] < jg2.posY+(jg2.tamanho[1]*0.20):
+                    b.moveUp = True
+                elif b.posY > jg2.posY+(jg2.tamanho[1]*0.80) :
+                    b.moveUp = False
+                else:
+                    b.moveUp = None
+                #sound1.play()
 
     jg1.velocidade = bola.velocidade*2
     jg2.velocidade = bola.velocidade*2
@@ -140,7 +170,7 @@ def atualizar():
 
 
 def inputs(): 
-    global jg1,jg2,run,pause       
+    global jg1,jg2,run,pause,timeBall      
     keys = pygame.key.get_pressed()
     if keys[K_p]:
         pause = not pause
@@ -159,12 +189,17 @@ def inputs():
         run = False
 
     if keys[K_n]:
-        print(len(lstBola))
+        print(len(lstBola),timeBall)
+        timeBall += 1
         if clock.get_fps() > 57:
             bola = Bola()
             bola.cor = (random.randint(1,255),random.randint(1,255),random.randint(1,255))
             bola.posInicial()
-            lstBola.append(bola)
+            if timeBall > 20:
+                lstBola.append(bola)
+                timeBall = 0
+        else:
+            del lstBola[0]
 
 def desenhar():
     jg1.desenhar()
@@ -172,8 +207,8 @@ def desenhar():
     for b in lstBola:
         b.desenhar()
     painel()
-    
-    
+    if clock.get_fps() < 57 and  len(lstBola) > 200:
+        del lstBola[0]
 
 pygame.init()
 fontPainel = pygame.font.SysFont("tahoma", 18)
